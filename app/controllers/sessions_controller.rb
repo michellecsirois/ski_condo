5b01c585d5e916a46134aac83f54ace9f33d5449
Eli Fatsi <eli.fatsi@viget.com>
2013-10-06 18:16:12 -0600

    Can log in and out

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    if user = User.find_by_email(params[:session][:email])
+      session[:user_id] = user.id
+      redirect_to root_url, :notice => 'You have successfully logged in!'
+    else
+      flash.now[:error] = 'That email address was not found'
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, :notice => 'You are now logged out.'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ SkiCondo::Application.routes.draw do
   resources :users
   resources :days
 
+  get '/login'  => 'sessions#new'
+  get '/logout' => 'sessions#destroy'
+
+  resources :sessions, :only => :create
+
   root 'days#index'
 end
